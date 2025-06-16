5f6b614b3b36dc0c48c5c977c10c2e32d5b7d6d7
idityaGE <am44910606@gmail.com>
2025-06-16 15:10:37 +0530

    refactor: restructure MDXLayout component for improved readability and maintainability

--- app/(content)/blogs/layout.tsx
+++ app/(content)/blogs/layout.tsx
@@ -7,12 +7,15 @@ const MDXLayout = ({
   children: React.ReactNode
 }) => {
   return (
-    <div className="w-full mx-auto max-w-3xl mt-20 px-3.5 md:px-0">
-      {children}
-      <div className='flex justify-center mt-8 mb-16 md:mb-32 '>
-        <SeeAllBlogs />
+    <>
+      <div className="fixed inset-0 bg-background/95 -z-5"></div>
+      <div className="w-full mx-auto max-w-3xl mt-20 px-3.5 md:px-0">
+        {children}
+        <div className='flex justify-center mt-8 mb-16 md:mb-32 '>
+          <SeeAllBlogs />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 

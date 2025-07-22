dd70dcb05e87ac811dd298bbf7d8714fd9afba04
idityaGE <am44910606@gmail.com>
2025-07-22 16:25:40 +0530

    feat: update Layout component to align items to the start and add To-Do List page

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col min-h-screen max-w-4xl items-center mx-auto">
+    <div className="flex flex-col min-h-screen max-w-4xl items-start mx-auto">
       <div className="flex-grow mt-20 md:mt-20 p-3 md:p-5">
         {children}
       </div>
---
+++ app/(landing)/todo/page.tsx
@@ -0,0 +1,11 @@
+
+const ToDoPage = () => {
+  return (
+    <div className="px-4 w-full">
+      <h1 className="text-2xl font-bold mb-4">To-Do List</h1>
+      <p className=" mb-6">This is a simple to-do list application.</p>
+    </div>
+  )
+}
+
+export default ToDoPage

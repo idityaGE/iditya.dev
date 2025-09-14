77e07d7edc7bbdb2ca30dd2484b5bdbabb52982e
idityaGE <am44910606@gmail.com>
2025-09-14 16:43:53 +0530

    feat: add ArrowUpRight icon to Projects component for enhanced visual indication

--- components/landing/cards/project.tsx
+++ components/landing/cards/project.tsx
@@ -1,5 +1,6 @@
 import { Ripple } from "@/components/magicui/ripple";
 import Link from "next/link";
+import { ArrowUpRight } from 'lucide-react'
 
 export function Projects() {
   return (
@@ -10,6 +11,9 @@ export function Projects() {
       <div className="relative flex h-56 w-full flex-col items-center justify-center md:shadow-xl hover:scale-125 hover:text-6xl duration-300 transform-gpu ease-in-out">
         <div className="flex items-center gap-2">
           <span>Projects</span>
+          <div className="group-hover:opacity-100 group-hover:translate-x-0 opacity-0 -translate-x-2 transition-all duration-300">
+            <ArrowUpRight className="h-8 w-8 md:h-10 md:w-10" />
+          </div>
         </div>
         <Ripple />
       </div>

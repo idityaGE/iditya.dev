d160669548443ed3f27aca4fd746a12e0e5481d0
idityaGE <am44910606@gmail.com>
2025-12-24 22:50:35 +0530

    selection color is new

--- components/landing/bento.tsx
+++ components/landing/bento.tsx
@@ -22,25 +22,29 @@ const features = [
     name: "Project",
     isIconHidden: true,
     background: <Projects />,
-    className: "lg:col-start-1 lg:col-end-5 lg:row-start-1 lg:row-end-2 border-b",
+    className:
+      "lg:col-start-1 lg:col-end-5 lg:row-start-1 lg:row-end-2 border-b",
   },
   {
     Icon: Link,
     name: "Connect",
     background: <ConnectCard />,
-    className: "lg:col-start-1 lg:col-end-3 lg:row-start-2 lg:row-end-3 border-b border-r",
+    className:
+      "lg:col-start-1 lg:col-end-3 lg:row-start-2 lg:row-end-3 border-r-0 md:border-r border-b",
   },
   {
     Icon: BookIcon,
     name: "Blogs",
     background: <BlogCard />,
-    className: "lg:col-start-3 lg:col-end-5 lg:row-start-2 lg:row-end-4 border-b",
+    className:
+      "lg:col-start-3 lg:col-end-5 lg:row-start-2 lg:row-end-4 border-b",
   },
   {
     Icon: MapPin,
     name: `${PersonalData.address.city}, ${PersonalData.address.country}`,
     background: <GlobeCard />,
-    className: "lg:row-start-4 lg:row-end-5 lg:col-start-3 lg:col-end-5",
+    className:
+      "lg:row-start-4 lg:row-end-5 lg:col-start-3 lg:col-end-5 border-b md:border-none",
   },
   // {
   //   Icon: LucideSheet,
@@ -52,7 +56,8 @@ const features = [
     Icon: Layers,
     name: "Tech Stack",
     background: <SkillCard />,
-    className: "lg:col-start-1 lg:col-end-3 lg:row-start-3 lg:row-end-5 border-r",
+    className:
+      "lg:col-start-1 lg:col-end-3 lg:row-start-3 lg:row-end-5 border-r-0 md:border-r md:border-r-[rgb(0_0_0/0.15)] md:dark:border-r-[rgb(255_255_255/0.18)]",
   },
 ];
 
